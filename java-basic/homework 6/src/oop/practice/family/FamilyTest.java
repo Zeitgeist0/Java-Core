181915eac2371df038da19aa4f44cf51616d6efd
Dariy Pavlyk <dariypavlyk@gmail.com>
2022-01-04 03:32:46 +0200

    tests updated

--- java-basic/homework 6/src/oop/practice/family/FamilyTest.java
+++ java-basic/homework 6/src/oop/practice/family/FamilyTest.java
@@ -8,28 +8,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FamilyTest {
   private Family family;
-  private Human human1;
-  private Human human2;
-  private Human human3;
-  private Human human4;
-  private Pet pet;
+  private Man man1;
+  private Woman woman1;
+  private Man man2;
+  private Woman woman2;
+  private DomesticCat domesticCat;
   @BeforeEach
   void setUp() {
-    human1 = new Human("John", "Doe", 1997, 106);
-    human2 = new Human("Margot" , "Robbie", 1985, 120);
-    human3 = new Human("Lisa", "Simpson", 1990, 145);
-    human4 = new Human("Bart", "Simpson", 1990,65 );
-//    pet = new Pet(Species.BIRD, "Bubi", 5,90);
-    family = new Family(human2, human1);
+    man1 = new Man("John", "Doe", 1997, 106);
+    woman1 = new Woman("Margot" , "Robbie", 1985, 120);
+    man2 = new Man("Lisa", "Simpson", 1990, 145);
+    woman2 = new Woman("Bart", "Simpson", 1990,65 );
+    domesticCat = new DomesticCat( "Bubi", 5,90);
+    family = new Family(woman1, man1);
   }
   @AfterEach
   void tearDown () {
     family = null;
-    human1 = null;
-    human2 = null;
-    human3 = null;
-    human4 = null;
-    pet = null;
+    man1 = null;
+    woman1 = null;
+    man2 = null;
+    woman2 = null;
+    domesticCat = null;
   }
 
 //  @Test
@@ -44,63 +44,63 @@ class FamilyTest {
   @Test
   public void anElementIsAddedToTheChildrensArray () {
     int initialChilds = family.getChildren().length;
-    family.addChild(human3);
+    family.addChild(man2);
     assertEquals(initialChilds + 1, family.getChildren().length );
   }
   @Test
   public void theCorrectChildIsAddedToTheArray () {
-    family.addChild(human3);
+    family.addChild(man2);
     Human[] childsArray = family.getChildren();
-    assertEquals(human3, childsArray[family.getChildren().length - 1]);
+    assertEquals(man2, childsArray[family.getChildren().length - 1]);
   }
 
 
 @Test
   public void childIsDeletedWhenPassingValidIndex() {
     int validRemoveIndex = 1;
-    family.addChild(human3);
-    family.addChild(human4);
+    family.addChild(man2);
+    family.addChild(woman2);
   assertTrue(family.deleteChild(validRemoveIndex));
     Human[] childsArray = family.getChildren();
-  assertNotEquals(human4, childsArray[validRemoveIndex - 1]);
+  assertNotEquals(woman2, childsArray[validRemoveIndex - 1]);
 
 }
   @Test
   public void childIsNotDeletedWhenPassingNotExistingIndex() {
     int invalidRemoveIndex = 5;
     int actualChildIndex = 1;
-    family.addChild(human3);
-    family.addChild(human4);
+    family.addChild(man2);
+    family.addChild(woman2);
     int initialChildsLength = family.getChildren().length;
     assertFalse(family.deleteChild(invalidRemoveIndex));
     Human[] childsArray = family.getChildren();
     int finalChildsLength = family.getChildren().length;
-    assertEquals(human4, childsArray[actualChildIndex]);
+    assertEquals(woman2, childsArray[actualChildIndex]);
     assertEquals(initialChildsLength, finalChildsLength);
   }
   @Test
   public void childIsDeletedWhenPassingExistingChild() {
-    family.addChild(human3);
-    family.addChild(human4);
+    family.addChild(man2);
+    family.addChild(woman2);
     int initialChildsLength = family.getChildren().length;
-    assertTrue(family.deleteChild(human4));
+    assertTrue(family.deleteChild(woman2));
     int finalChildsLength = family.getChildren().length;
     assertNotEquals(initialChildsLength, finalChildsLength);
   }
   @Test
   public void childIsNotDeletedWhenPassingNotExistingChild() {
-    family.addChild(human3);
-    family.addChild(human4);
+    family.addChild(man2);
+    family.addChild(woman2);
     int initialChildsLength = family.getChildren().length;
-    assertFalse(family.deleteChild(human1));
+    assertFalse(family.deleteChild(man1));
     int finalChildsLength = family.getChildren().length;
     assertEquals(initialChildsLength, finalChildsLength);
   }
   @Test
   public void childsAreProperlyCounted() {
     int initialChildsLength = family.getChildren().length;
-    family.addChild(human3);
-    family.addChild(human4);
+    family.addChild(man2);
+    family.addChild(woman2);
     int finalChildsLength = family.getChildren().length;
     assertEquals(finalChildsLength, family.countFamily());
     assertEquals(initialChildsLength + 2, family.countFamily());
